75eacdd5fd644d452398875694250f2399734848
Mohammad Moslemi <mhm@london-2-embody-ai-node-5.london2.huaweicudo.lan>
2026-06-04 16:21:36 +0000

    add new pt

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SCRIPTS=(main.py main_DM.py)
+# SCRIPTS=(main.py main_DM.py)
+SCRIPTS=(main_DM.py)
 DATASETS=(SVHN FashionMNIST STL10)
 IPCS=(10 50 100)
 
@@ -8,7 +9,7 @@ for script in "${SCRIPTS[@]}"; do
     for dataset in "${DATASETS[@]}"; do
         for ipc in "${IPCS[@]}"; do
             echo "=== Running: python $script --dataset $dataset --ipc $ipc ==="
-            python "$script" --dataset "$dataset" --ipc "$ipc"
+            CUDA_VISIBLE_DEVICES=6 python "$script" --dataset "$dataset" --ipc "$ipc"
         done
     done
 done
