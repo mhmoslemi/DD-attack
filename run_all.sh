52eb4dd484c7019e604d85915a3ba72a3320116f
mohammad <mhmoslemi2338@gmail.com>
2026-06-04 13:48:04 -0400

    new pt

--- run_all.sh
+++ run_all.sh
@@ -2,14 +2,17 @@
 
 # SCRIPTS=(main.py main_DM.py)
 SCRIPTS=(main_DM.py)
-DATASETS=(SVHN FashionMNIST STL10)
-IPCS=(10 50 100)
+# DATASETS=(SVHN FashionMNIST STL10)
+DATASETS=(STL10)
+# IPCS=(10 50 100)
+# IPCS=(50 100)
+IPCS=(10)
 
 for script in "${SCRIPTS[@]}"; do
     for dataset in "${DATASETS[@]}"; do
         for ipc in "${IPCS[@]}"; do
             echo "=== Running: python $script --dataset $dataset --ipc $ipc ==="
-            CUDA_VISIBLE_DEVICES=6 python "$script" --dataset "$dataset" --ipc "$ipc"
+            python "$script" --dataset "$dataset" --ipc "$ipc"
         done
     done
 done
