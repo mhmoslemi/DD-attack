1b0e9ff7aae5a2636a31801ae9b8666c96226f7b
mohammad <mhmoslemi2338@gmail.com>
2026-06-09 13:37:03 -0400

    add

--- select.sh
+++ select.sh
@@ -25,17 +25,17 @@
 
 
 
-CUDA_VISIBLE_DEVICES=4 python eval_standard_nodistill.py \
+python eval_standard_nodistill.py \
     --syn_data_path result/res_DM_CIFAR10_ConvNet_50ipc.pt \
     --surrogate_model ConvNet --model ConvNetBN \
     --class_pairs dog-bird \
     --attack gradmatch --restarts 8 \
-    --budget 0.01 --epsilon 0.0313725 --pgd_steps 250 --pgd_alpha 0.0039216 \
-    --lambda_margin 1.0 \
+    --budget 0.005 --epsilon 0.0313725 --pgd_steps 150 --pgd_alpha 0.0039216 \
+    --lambda_margin 0.1 \
     --num_surrogates 10 --surrogate_epochs 1000 \
-    --num_targets 4 --num_victims 5 \
+    --num_targets 5 --num_victims 6 \
     --victim_epochs 60 --victim_lr 0.1 --victim_bs 125 --victim_decay 40 \
-    --target_select random --seed 0 --clean_baseline
+    --target_select random --seed 0 
 
 # echo "===== Cell A: plain FC (random select + single surrogate) ====="
 # $COMMON --random_select --single_surrogate --out_dir result/abl_A_plain_fc
@@ -48,3 +48,11 @@ CUDA_VISIBLE_DEVICES=4 python eval_standard_nodistill.py \
 
 # echo "===== Cell D: full method (scored select + full ensemble) ====="
 # $COMMON --out_dir result/abl_D_full_method
+
+
+# for grad match
+# margin 10  --> ASR = 43.3%
+# margin 2.0 --> ASR = 43.3%
+# margin 1.0 --> ASR = 46.7%
+# margin 0.1 --> ASR = 23.3%
+
